429b15da9d01176cc68a52f506e98b594ba043df
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-23 13:18:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,3 @@ else
   array[0...-1].join(", ") << ", and #{array[-1]}"
 end 
 end  
-
-#begin if array.length == 1 || 2
- # return array.join(" and ")
- # elsif array.length == 3
- # return array.split(",") 
