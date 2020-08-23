506c26b93e3fd5546a3d1d2e9d32a0ff16fd8efe
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-23 13:17:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,16 @@
 def oxford_comma(array)
-if array.length == 1 || 2
+  case array.length 
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
+
+=begin if array.length == 1 || 2
   return array.join(" and ")
   elsif array.length == 3
   return array.split(",")
-end
-end
+  =end 
